04c092c47db773de603e264354b0085ceef64c4b
Kip <klim@hmc.edu>
2020-06-07 17:46:38 -0700

    update links

--- TaskApp/App.js
+++ TaskApp/App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
-import useCachedResources from './app/hooks/useCachedResources';
+import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './app/navigation/BottomTabNavigator';
 import LinkingConfiguration from './app/navigation/LinkingConfiguration';
 
