5370e1c89d84b5cbf8c26ff5e995c2c1d1392da2
theyingster <theyingstor@gmail.com>
2020-06-08 20:41:25 -0700

    test
    Co-authored-by: Allen Wu <allenwu283@gmail.com>

--- TaskApp/app/screens/HomeScreen.js
+++ TaskApp/app/screens/HomeScreen.js
@@ -3,18 +3,40 @@ import {
   StyleSheet,
   Text, 
   View, 
-  ImageBackground
+  ImageBackground, 
+  Image,
+  Button
 } from 'react-native';
-import backgroundImg from './assets/images/homescreenBackground.png'
+import backgroundImg from './assets/images/homescreenBackground.jpg';
+//import logo from './assets/images/blackbox.png';
 
 export default class HomeScreen extends Component {
   render() {
     return (
       <ImageBackground source={backgroundImg} style={StyleSheet.backgroundContainer}>
-        
+        <View>
+          <Image source={logo} style={StyleSheet.logo} />
+          {/* <Button
+          title="Get Started"
+          onPress={() => handlePress()}
+          /> */}
+          <Button
+            title="Go to Jane's profile"
+            onPress={() =>
+              navigation.navigate('Profile', { name: 'Jane' })
+            }
+          />
+        </View>
+
       </ImageBackground>
     )
   }
+}
+
+function handlePress(){
+  setScreen(prevElements => {
+    return prevElements.filter(element => element.id == 'something')
+  });
 }
 
 const styles = StyleSheet.create ({
@@ -24,5 +46,9 @@ const styles = StyleSheet.create ({
     height: null,
     justifyContent: 'center',
     alignItems: 'center', 
+  },
+  logo: {
+    width: 120,
+    height: 120,
   }
 })
