6647d68ba65da6114ca33cfd5aad8bd2b84b8b66
allenwu038 <allenwu283@gmail.com>
2020-06-10 22:01:24 -0700

    new background image, MakeProfile now has scroll function for choosing character, and name input box

--- TaskApp/app/screens/MakeProfile.jsx
+++ TaskApp/app/screens/MakeProfile.jsx
@@ -1,23 +1,66 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import {
     StyleSheet,
-    Text, 
     View, 
     ImageBackground, 
     Image,
-    Button
+    Button, 
   } from 'react-native';
+import { SliderBox } from "react-native-image-slider-box";
 import backgroundImg from '../assets/images/homescreenBackground.jpg';
+import { Text, TextInput } from 'react-native';
+import  PizzaTranslator  from './TextInput.jsx';
 
-export default class MakeProfile extends React.Component {
-    render() {
-        return (
-        <ImageBackground source={backgroundImg} style={styles.backgroundContainer}>
+export default class MakeProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [
+        "https://source.unsplash.com/1024x768/?nature",
+        "https://source.unsplash.com/1024x768/?water",
+        "https://source.unsplash.com/1024x768/?girl", // Network image
+      ]
+    };
+  }
 
-        </ImageBackground>
+  state = {
+    value: '',
+  }
 
-        )  
-    }
+  handleTextChange = (newText) => this.ListeningStateChangedEvent({value: newText})
+
+  render() {
+      return (  
+          <ImageBackground source={backgroundImg} style={styles.backgroundContainer}>
+            {/* https://www.npmjs.com/package/react-native-image-slider-box*/}
+            
+            <Text style={{ fontSize: 14, color: '#000', marginTop:300}}>
+              Enter Your Name:
+            </Text>
+            <TextInput 
+              value={this.state.value}
+              onChangetext={this.handleTextChange}
+              style={{ height: 26, fontSize:20, color: '#000', borderBottomWidth: 1, borderBottomColor: '#555', marginTop: 70 }}
+            />
+            <Text style={{ fontSize: 14, color: '#000', marginTop: 100, padding: 0}}>
+              Choose an avatar (scroll left/right):
+            </Text>
+            <SliderBox 
+                images={this.state.images}
+                //onCurrentImagePressed={index => console.warn(`character ${index} selected. Proceed?`)}
+                dotColor="#FFEE58"
+                sliderBoxHeight = {1000}
+                inactiveDotColor="#90A4EA"
+                circleLoop
+                resizeMethod={'resize'}
+                resizeMode={'cover'}
+                dotStyle = { {width: 10, height: 10, marginBottom: 450} }
+                ImageComponentStyle ={ {width: 225, height: 225, borderRadius: 225, marginTop: 50} }
+            />
+          </ImageBackground>
+        
+      )  
+  }
 }
 
 const styles = StyleSheet.create ({
@@ -28,5 +71,7 @@ const styles = StyleSheet.create ({
       justifyContent: 'center',
       alignItems: 'center', 
     },
+    inputLayout: {
+      paddingTop: 100,
     }
-  )
+  });
---
+++ TaskApp/app/screens/TextInput.jsx
@@ -0,0 +1,19 @@
+import React, { Component, useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+export default function PizzaTranslator() {
+  const [text, setText] = useState('');
+  return (
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
+    </View>
+  );
+}
