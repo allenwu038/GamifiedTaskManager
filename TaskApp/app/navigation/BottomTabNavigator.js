4b276d7658f30b2e81275dcf1d2aeb097539c4f1
Kip <klim@hmc.edu>
2020-06-11 18:52:36 -0700

    added quests and tasks, removed home and links

--- TaskApp/app/navigation/BottomTabNavigator.js
+++ TaskApp/app/navigation/BottomTabNavigator.js
@@ -5,9 +5,11 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import LoginScreen from '../screens/LoginScreen';
+import TaskScreen from "../screens/TaskScreen";
+import QuestScreen from "../screens/QuestScreen";
 
 const BottomTab = createBottomTabNavigator();
-const INITIAL_ROUTE_NAME = 'Home';
+const INITIAL_ROUTE_NAME = 'Login';
 
 export default function BottomTabNavigator({ navigation, route }) {
   // Set the header title on the parent stack navigator depending on the
@@ -18,19 +20,23 @@ export default function BottomTabNavigator({ navigation, route }) {
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
-        name="Login"
-        component={LoginScreen}
+        name="Tasks"
+        component={TaskScreen}
         options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-code-working" />,
+          title: "To-Do",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-code-working" />
+          ),
         }}
       />
       <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
+        name="Quests"
+        component={QuestScreen}
         options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+          title: "Quests",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name="md-book" />
+          ),
         }}
       />
     </BottomTab.Navigator>
@@ -41,9 +47,11 @@ function getHeaderTitle(route) {
   const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
   switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
+    case 'Login':
+      return 'Make a profile';
+    case 'Tasks':
+      return 'Get some shit done';
+    case 'Quests':
+      return 'Rack up rewardz';
   }
 }
--- TaskApp/app/navigation/LinkingConfiguration.js
+++ TaskApp/app/navigation/LinkingConfiguration.js
@@ -6,8 +6,8 @@ export default {
     Root: {
       path: 'root',
       screens: {
-        Home: 'home',
-        Links: 'links',
+        Quests: "quests",
+        Tasks: "tasks",
       },
     },
   },
