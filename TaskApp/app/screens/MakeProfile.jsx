8a0e006bcc911c9e328f2ab6d776c27995f8df31
allenwu038 <allenwu283@gmail.com>
2020-06-16 21:13:45 -0700

    text input box, rectangle to cover image black background, 'next' button but no navigation

--- TaskApp/app/screens/MakeProfile.jsx
+++ TaskApp/app/screens/MakeProfile.jsx
@@ -9,7 +9,12 @@ import {
 import { SliderBox } from "react-native-image-slider-box";
 import backgroundImg from '../assets/images/homescreenBackground.jpg';
 import { Text, TextInput } from 'react-native';
-import  PizzaTranslator  from './TextInput.jsx';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
 
 export default class MakeProfile extends Component {
   constructor(props) {
@@ -34,20 +39,20 @@ export default class MakeProfile extends Component {
           <ImageBackground source={backgroundImg} style={styles.backgroundContainer}>
             {/* https://www.npmjs.com/package/react-native-image-slider-box*/}
             
-            <Text style={{ fontSize: 14, color: '#000', marginTop:300}}>
+            <Text style={styles.text}>
               Enter Your Name:
             </Text>
             <TextInput 
               value={this.state.value}
               onChangetext={this.handleTextChange}
-              style={{ height: 26, fontSize:20, color: '#000', borderBottomWidth: 1, borderBottomColor: '#555', marginTop: 70 }}
+              style={styles.textInput}
             />
             <Text style={{ fontSize: 14, color: '#000', marginTop: 100, padding: 0}}>
-              Choose an avatar (scroll left/right):
+              Scroll along the dots to choose an avatar:
             </Text>
             <SliderBox 
                 images={this.state.images}
-                //onCurrentImagePressed={index => console.warn(`character ${index} selected. Proceed?`)}
+                //onCurrentImagePressed={index => handlePress(index)}
                 dotColor="#FFEE58"
                 sliderBoxHeight = {300}
                 inactiveDotColor="#90A4EA"
@@ -55,13 +60,28 @@ export default class MakeProfile extends Component {
                 resizeMethod={'resize'}
                 resizeMode={'cover'}
                 dotStyle = { {width: 15, height: 15, marginBottom: 450} }
-                ImageComponentStyle ={ {width: 225, height: 225, borderRadius: 225, marginTop: 50} }
+                ImageComponentStyle ={ {width: 225, height: 225, borderRadius: 225, marginTop: 55} }
+                
             />
+            <View style={styles.rectangle}/>
+            <View style={styles.bottom}>
+              <Button
+                
+                title="Next"
+                onPress={() => changeScreen({ navigation })}
+              />
+            </View>
           </ImageBackground>
-        
       )  
   }
 }
+
+function changeScreen({ navigation }) {
+  return (
+    navigation.navigate('TaskScreen')
+  );
+}
+
 
 const styles = StyleSheet.create ({
     backgroundContainer: {
@@ -73,5 +93,34 @@ const styles = StyleSheet.create ({
     },
     inputLayout: {
       paddingTop: 100,
-    }
+    },
+    text: { 
+      fontSize: 14, 
+      color: '#000', 
+      marginTop:300,
+    },
+    textInput: {
+      height: 50, 
+      borderWidth: 1, 
+      textAlign: 'center', 
+      borderColor: '#777', 
+      padding: 8, 
+      margin: 10, 
+      width:200,
+     },
+     rectangle: {
+      position: 'absolute',
+      width: 500,
+      height: 315,
+      color: '#000',
+    },
+    bottom: {
+      position: 'absolute',
+      padding: 20,
+      flex: 1,
+      bottom: 0,
+      width: 500,
+      backgroundColor: '#FFEE58',
+    },
   });
+
