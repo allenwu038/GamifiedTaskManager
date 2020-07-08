e9eaf031d36b4ed20cc36d4769d1a7fd653fc25d
allenwu038 <allenwu283@gmail.com>
2020-07-07 21:21:32 -0700

    Revert "image error"
    This reverts commit 4e14b464f9eec99c0fd7a4406f6dbaa675a9cb18.

--- TaskApp/app/components/levels/LevelList.jsx
+++ TaskApp/app/components/levels/LevelList.jsx
@@ -13,49 +13,49 @@ import "react-native-interactive-image";
 let maxLevel = 10;
 let currentLevel = 3;
 
-//function Levels(){
-//     const annotations = [
-//         {
-//             x1: 25,
-//             x2: 35,
-//             y1: 20,
-//             y2: 30,
-//             description: 'Chapter 1: Bread',
-//         },
-//         {
-//             x1: 60,
-//             x2: 70,
-//             y1: 15,
-//             y2: 25,
-//             description: 'Chapter 2: Muffin',
-//         },
-//         {
-//             x1: 20,
-//             x2: 30,
-//             y1: 50,
-//             y2: 60,
-//             description: 'Chapter 3: Cheese',
-//         },
-//         {
-//             x1: 65,
-//             x2: 75,
-//             y1: 65,
-//             y2: 75,
-//             description: 'Chapter 4: Pizza',
-//         },
-//     ]
-//     return (
-//         <ZoomableImage
-// 					//source={ require('../../assets/images/levelsbackground.jpg') }
-// 					imageHeight={ 500 }
-// 					imageWidth={ 500 }
-// 					annotations={ annotations }
-// 					// popOverStyles={ { backgroundColor: 'white' } }
-//         />
-//     );
-// }
+export default function LevelList(){
+    const annotations = [
+        {
+            x1: 25,
+            x2: 35,
+            y1: 20,
+            y2: 30,
+            description: 'Chapter 1: Bread',
+        },
+        {
+            x1: 60,
+            x2: 70,
+            y1: 15,
+            y2: 25,
+            description: 'Chapter 2: Muffin',
+        },
+        {
+            x1: 20,
+            x2: 30,
+            y1: 50,
+            y2: 60,
+            description: 'Chapter 3: Cheese',
+        },
+        {
+            x1: 65,
+            x2: 75,
+            y1: 65,
+            y2: 75,
+            description: 'Chapter 4: Pizza',
+        },
+    ]
+    return (
+        <ZoomableImage
+					source={ require('../assets/images/levelsBackground.jpg') }
+					imageHeight={ 600 }
+					imageWidth={ 450 }
+					annotations={ annotations }
+					// popOverStyles={ { backgroundColor: 'white' } }
+        />
+    );
+}
 
-export default function LevelList() {
+function Levels() {
     let levels = [];
     for (let i = 0; i < maxLevel; i++) {
         let temp = i < currentLevel-1 ? true : false;
