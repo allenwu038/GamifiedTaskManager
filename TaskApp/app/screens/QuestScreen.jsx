65ec488ac5cb4d484eb0ed6944619652529bb4e9
allenwu038 <allenwu283@gmail.com>
2020-07-09 20:29:06 -0700

    Revert "trying interactive image"
    This reverts commit ebcd86bf9284529a14786cf1d75456ed90088849.

--- TaskApp/app/screens/QuestScreen.jsx
+++ TaskApp/app/screens/QuestScreen.jsx
@@ -13,51 +13,13 @@ import { ScrollView } from "react-native-gesture-handler";
 
 import { MonoText } from "../components/StyledText";
 import LevelList from "../components/levels/LevelList.jsx";
-//import levelsBackground from '../assets/images/homescreenBackground.jpg';
+import levelsBackground from '../assets/images/homescreenBackground.jpg';
 
 export default function QuestScreen() {
-  const annotations = [
-    {
-      x1: 25,
-      x2: 35,
-      y1: 20,
-      y2: 30,
-      description: 'A pair of black running sports shoes, has lace-up detail. Textile and mesh upper',
-    },
-    {
-      x1: 60,
-      x2: 70,
-      y1: 15,
-      y2: 25,
-      description: 'Shoe sole tip!',
-    },
-    {
-      x1: 20,
-      x2: 30,
-      y1: 50,
-      y2: 60,
-      description: 'Textured and patterned outsole',
-    },
-    {
-      x1: 65,
-      x2: 75,
-      y1: 65,
-      y2: 75,
-      description: 'Textured outsole with a stacked heel',
-    },
-  ]
-
   return (
-      <ZoomableImage
-					source={ require('../assets/images/homescreenBackground.jpg') }
-					imageHeight={ 600 }
-					imageWidth={ 450 }
-					annotations={ annotations }
-					popOverStyles={ { backgroundColor: 'white' } }
-      />
-    // {/* <ImageBackground source={levelsBackground} style={styles.backgroundContainer}>  
-    //   <LevelList/>
-    // </ImageBackground> */}
+    <ImageBackground source={levelsBackground} style={styles.backgroundContainer}>
+        <LevelList/>
+    </ImageBackground>
   );
 }
 
